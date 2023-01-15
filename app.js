839248dc30ff0d605b6830f33ac0b767401ac46a
aguerosergiokun <donginoffcl@proton.me>
2023-01-15 22:10:06 +0900

    draw something

---
+++ app.js
@@ -0,0 +1,24 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+let isPainting = false;
+canvas.width = 800;
+canvas.height = 800;
+ctx.lineWidth = 1;
+function onMove(event) {
+    if (isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+function startPainting() {
+    isPainting = true;
+}
+function cancelPainting() {
+    isPainting = false;
+}
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
